a8504c1e72a944d650674bb31cd8f5409685a2db
eparubrub <emmit.parubrub@gmail.com>
2021-11-06 15:10:47 -0700

    adding services text

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
       <div className="app-container">
         <Navbar/>
+
         <div className="flex-container">
-          <div className="flex-item main-card-desc">
+          <div className="flex-item flex-item-large">
             <div className="main-description">
               <h1>EPK Plumbing</h1>
               <h3>Plumbing, Drain Cleaning, Flooring, Painting</h3>
@@ -23,17 +24,66 @@ function App() {
               <span className="link-text">email@gmail.com</span>
             </div>
             <div className="main-button">
-            <img className="link-png" src={"/images/phone.png"} alt="mail png"/>
-              <span className="link-text">(808) 123-4567</span>
+              <img className="link-png" src={"/images/phone.png"} alt="phone png"/>
+                <span className="link-text">(808) 123-4567</span>
             </div>
             <div className="main-button">
-            <img className="link-png" src={"/images/yelp.png"} alt="mail png"/>
-              <span className="link-text">Yelp Business Page</span>
+              <img className="link-png" src={"/images/yelp.png"} alt="yelp png"/>
+                <span className="link-text">Yelp Business Page</span>
             </div>
           </div>
-          <div className="flex-item main-card-img">
+          <div className="flex-item flex-item-small">
             <img className="main-img" src={"/images/MainImg.jpg"} alt="main business pic"/>
-            </div>
+          </div>
+        </div>
+
+        <div className="flex-container">
+          <div className="flex-item flex-item-small">
+          </div>
+          <div className="flex-item flex-item-large ">
+            <h1 className="available-services-title">Available Services</h1>
+            <ul className="available-services-container">
+              <li class="available-services-item">
+                <h3 className="available-services-sub-item-title">Plumbing</h3>
+                <li className="available-services-sub-item">Drain Installation & Cleaning</li>
+                <li className="available-services-sub-item">Water Heater Installation & Repair</li>
+                <li className="available-services-sub-item">Sink Installation & Repair</li>
+                <li className="available-services-sub-item">Pipe Installation & Repair</li>
+                <li className="available-services-sub-item">Faucet Installation & Repair</li>
+                <li className="available-services-sub-item">Leak Detection</li>
+                <li className="available-services-sub-item">Garbage Disposal Installation & Repair</li>
+              </li>
+              <li class="available-services-item">
+                <h3 className="available-services-sub-item-title">Flooring</h3>
+                <li className="available-services-sub-item">Laminate Floor Repair</li>
+                <li className="available-services-sub-item">Tile Floor Installation & Restoration</li>
+                <li className="available-services-sub-item">Tile Floor Repair</li>
+                <li className="available-services-sub-item">Vinyl & Linoleum Floor Repair</li>
+                <li className="available-services-sub-item">Vinyl & Linoleum Floor Installation</li>
+                <li className="available-services-sub-item">Vinyl & Linoleum Floor Restoration</li>
+                <li className="available-services-sub-item">Concrete Floor Repair</li>
+                <li className="available-services-sub-item">Laminate Floor Installation</li>
+              </li>
+              <li class="available-services-item">
+                <h3 className="available-services-sub-item-title">Bathroom & Other</h3>
+                <li className="available-services-sub-item">Toilet Installation & Repair</li>
+                <li className="available-services-sub-item">Bathtub Installation & Repair</li>
+                <li className="available-services-sub-item">Shower Installation & Repair</li>
+                <li className="available-services-sub-item">Residential Services</li>
+                <li className="available-services-sub-item">Water Filter Services</li>
+                <li className="available-services-sub-item">Water Heater Repair</li>
+              </li>
+              <li class="available-services-item">
+                <h3 className="available-services-sub-item-title">Painting</h3>
+                <li className="available-services-sub-item">Ceiling & Wall Painting</li>
+                <li className="available-services-sub-item">Color Matching</li>
+                <li className="available-services-sub-item">Interior Painting</li>
+                <li className="available-services-sub-item">Exterior Painting</li>
+                <li className="available-services-sub-item">Staining & Varnishing</li>
+                <li className="available-services-sub-item">Trim & Baseboard Painting</li>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
   );
